567120bf9524d79329d8f1f798434a57778e18d8
Gabriele Peck <peck.gabriele@gmail.com>
2024-10-16 14:56:58 -0700

    added BLoC to order_form.dart and successfully got the number of small pizzas to increment or decrement using BLoC

--- lib/screens/order_form.dart
+++ lib/screens/order_form.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_chef/bloc/small_pizza/small_pizza_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_chef/bloc/small_pizza/small_pizza_event.dart';
+import 'package:pizza_chef/bloc/small_pizza/small_pizza_state.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(printer: PrettyPrinter());
 
 class OrderForm extends StatefulWidget {
   const OrderForm({super.key});
@@ -23,6 +30,8 @@ class _OrderFormState extends State<OrderForm> {
 
   @override
   Widget build(BuildContext context) {
+    final smallPizzaCounterBloc = context.read<SmallPizzaCounterBloc>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Start Order'),
@@ -34,62 +43,80 @@ class _OrderFormState extends State<OrderForm> {
             const Row(
               children: [Text('How many pizzas would you like?')],
             ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.remove),
-                ),
-                Expanded(
-                  child: TextFormField(
-                    controller: largePizzaController,
-                    decoration: const InputDecoration(
-                        label: Text('How many large pizzas?')),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.remove),
-                ),
-                Expanded(
-                  child: TextFormField(
-                    controller: mediumPizzaController,
-                    decoration: const InputDecoration(
-                        label: Text('How many medium pizzas?')),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.remove),
-                ),
-                Expanded(
-                  child: TextFormField(
-                    controller: smallPizzaController,
-                    decoration: const InputDecoration(
-                        label: Text('How many small pizzas?')),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add),
-                ),
-              ],
+            // Row(
+            //   children: [
+            //     IconButton(
+            //       onPressed: () {},
+            //       icon: const Icon(Icons.remove),
+            //     ),
+            //     Expanded(
+            //       child: TextFormField(
+            //         controller: largePizzaController,
+            //         decoration: const InputDecoration(
+            //             label: Text('How many large pizzas?')),
+            //       ),
+            //     ),
+            //     IconButton(
+            //       onPressed: () {},
+            //       icon: const Icon(Icons.add),
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   children: [
+            //     IconButton(
+            //       onPressed: () {},
+            //       icon: const Icon(Icons.remove),
+            //     ),
+            //     Expanded(
+            //       child: TextFormField(
+            //         controller: mediumPizzaController,
+            //         decoration: const InputDecoration(
+            //             label: Text('How many medium pizzas?')),
+            //       ),
+            //     ),
+            //     IconButton(
+            //       onPressed: () {},
+            //       icon: const Icon(Icons.add),
+            //     ),
+            //   ],
+            // ),
+            BlocBuilder<SmallPizzaCounterBloc, SmallPizzaCounterState>(
+              builder: (context, state) {
+                return Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        smallPizzaCounterBloc
+                            .add(SmallPizzaDecrement()); // add an event
+                        setState(() {
+                          smallPizzaController.text = state.count.toString();
+                        });
+                        logger.d(state.count);
+                      },
+                      icon: const Icon(Icons.remove),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: smallPizzaController,
+                        decoration: const InputDecoration(
+                            label: Text('How many small pizzas?')),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        smallPizzaCounterBloc
+                            .add(SmallPizzaIncrement()); // add an event
+                        setState(() {
+                          smallPizzaController.text = state.count.toString();
+                        });
+                        logger.d(state.count);
+                      },
+                      icon: const Icon(Icons.add),
+                    ),
+                  ],
+                );
+              },
             )
           ],
         ),
