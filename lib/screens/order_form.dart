66c42c14bb2c51ec4142792d037d68784e2d03df
Gabriele Peck <peck.gabriele@gmail.com>
2024-10-16 12:16:47 -0700

    created the beginning of an order form to facilitate ordering pizzas. it currently has three inputs for how many small, medium, and large pizzas the user would like to order

--- lib/screens/order_form.dart
+++ lib/screens/order_form.dart
@@ -9,6 +9,17 @@ class OrderForm extends StatefulWidget {
 
 class _OrderFormState extends State<OrderForm> {
   final _formKey = GlobalKey<FormState>();
+  final largePizzaController = TextEditingController();
+  final mediumPizzaController = TextEditingController();
+  final smallPizzaController = TextEditingController();
+
+  @override
+  void dispose() {
+    largePizzaController.dispose();
+    mediumPizzaController.dispose();
+    smallPizzaController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +27,71 @@ class _OrderFormState extends State<OrderForm> {
       appBar: AppBar(
         title: const Text('Start Order'),
       ),
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: const Column(
-            children: [
-              
-            ],
-          ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            const Row(
+              children: [Text('How many pizzas would you like?')],
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.remove),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    controller: largePizzaController,
+                    decoration: const InputDecoration(
+                        label: Text('How many large pizzas?')),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.remove),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    controller: mediumPizzaController,
+                    decoration: const InputDecoration(
+                        label: Text('How many medium pizzas?')),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.remove),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    controller: smallPizzaController,
+                    decoration: const InputDecoration(
+                        label: Text('How many small pizzas?')),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
