837bda4756702d5a97a6fe5b13b01a5efb1c3bf9
Gabriele Peck <peck.gabriele@gmail.com>
2024-10-16 14:55:17 -0700

    made MaterialApp the child of MultiBlocProvider instead of the other way around

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_chef/screens/home.dart';
+import 'package:pizza_chef/bloc/small_pizza/small_pizza_bloc.dart';
 
 void main() {
   runApp(
-    const MaterialApp(home: Home(),
-      debugShowCheckedModeBanner: false,
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => SmallPizzaCounterBloc(),
+        )
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        
+        home: Home(),
+      ),
     ),
   );
 }
