0e134017297c9f984a1ccf61edf356530e8bb4c1
Gabriele Peck <peck.gabriele@gmail.com>
2024-10-21 14:41:31 -0700

    added a checkbox list of all the toppings

--- lib/screens/order_form.dart
+++ lib/screens/order_form.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:pizza_chef/data/pizza_sauce.dart';
 import 'package:pizza_chef/data/pizza_size.dart';
 import 'package:pizza_chef/data/pizza_crust.dart';
+import 'package:pizza_chef/data/toppings.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(printer: PrettyPrinter());
 
 class OrderForm extends StatefulWidget {
   const OrderForm({super.key});
@@ -18,14 +22,7 @@ class _OrderFormState extends State<OrderForm> {
   PizzaSize? selectedSize;
   PizzaSauce? selectedSauce;
   PizzaCrust? selectedCrust;
-
-  @override
-  void dispose() {
-    sizeController.dispose();
-    sauceController.dispose();
-    crustController.dispose();
-    super.dispose();
-  }
+  List<String>? selectedToppings = [];
 
   @override
   Widget build(BuildContext context) {
@@ -37,71 +34,104 @@ class _OrderFormState extends State<OrderForm> {
         key: _formKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                DropdownMenu<PizzaSize>(
-                  initialSelection: PizzaSize.small,
-                  controller: sizeController,
-                  requestFocusOnTap: true,
-                  label: const Text('Pizza Size'),
-                  onSelected: (PizzaSize? size) {
-                    setState(() {
-                      selectedSize = size;
-                    });
-                  },
-                  dropdownMenuEntries: PizzaSize.values
-                      .map<DropdownMenuEntry<PizzaSize>>((PizzaSize size) {
-                    return DropdownMenuEntry<PizzaSize>(
-                      value: size,
-                      label: size.label,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DropdownMenu<PizzaSize>(
+                initialSelection: PizzaSize.small,
+                controller: sizeController,
+                requestFocusOnTap: true,
+                label: const Text('Pizza Size'),
+                onSelected: (PizzaSize? size) {
+                  setState(() {
+                    selectedSize = size;
+                  });
+                },
+                dropdownMenuEntries: PizzaSize.values
+                    .map<DropdownMenuEntry<PizzaSize>>((PizzaSize size) {
+                  return DropdownMenuEntry<PizzaSize>(
+                    value: size,
+                    label: size.label,
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 48),
+              DropdownMenu<PizzaSauce>(
+                initialSelection: PizzaSauce.red,
+                controller: sauceController,
+                requestFocusOnTap: true,
+                label: const Text('Pizza Sauce'),
+                onSelected: (PizzaSauce? sauce) {
+                  setState(() {
+                    selectedSauce = sauce;
+                  });
+                },
+                dropdownMenuEntries: PizzaSauce.values
+                    .map<DropdownMenuEntry<PizzaSauce>>((PizzaSauce sauce) {
+                  return DropdownMenuEntry<PizzaSauce>(
+                    value: sauce,
+                    label: sauce.label,
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 48),
+              DropdownMenu<PizzaCrust>(
+                initialSelection: PizzaCrust.thinCrust,
+                controller: crustController,
+                requestFocusOnTap: true,
+                label: const Text('Pizza Crust'),
+                onSelected: (PizzaCrust? crust) {
+                  setState(() {
+                    selectedCrust = crust;
+                  });
+                },
+                dropdownMenuEntries: PizzaCrust.values
+                    .map<DropdownMenuEntry<PizzaCrust>>((PizzaCrust crust) {
+                  return DropdownMenuEntry<PizzaCrust>(
+                    value: crust,
+                    label: crust.label,
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 48),
+              const Text('Scroll through the toppings:'),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: toppings.length,
+                  itemBuilder: (context, index) {
+                    return CheckboxListTile(
+                      tristate: false,
+                      title: Text(toppings[index]),
+                      value: selectedToppings?.contains(toppings[index]),
+                      onChanged: (bool? value) {
+                        setState(() {
+                          if (value == true) {
+                            selectedToppings?.add(toppings[index]);
+                          } else {
+                            selectedToppings?.remove(toppings[index]);
+                          }
+                        });
+                      },
                     );
-                  }).toList(),
-                ),
-                const SizedBox(height: 48),
-                DropdownMenu<PizzaSauce>(
-                  initialSelection: PizzaSauce.red,
-                  controller: sauceController,
-                  requestFocusOnTap: true,
-                  label: const Text('Pizza Sauce'),
-                  onSelected: (PizzaSauce? sauce) {
-                    setState(() {
-                      selectedSauce = sauce;
-                    });
                   },
-                  dropdownMenuEntries: PizzaSauce.values
-                      .map<DropdownMenuEntry<PizzaSauce>>((PizzaSauce sauce) {
-                    return DropdownMenuEntry<PizzaSauce>(
-                      value: sauce,
-                      label: sauce.label,
-                    );
-                  }).toList(),
                 ),
-                const SizedBox(height: 48),
-                DropdownMenu<PizzaCrust>(
-                  initialSelection: PizzaCrust.thinCrust,
-                  controller: crustController,
-                  requestFocusOnTap: true,
-                  label: const Text('Pizza Crust'),
-                  onSelected: (PizzaCrust? crust) {
-                    setState(() {
-                      selectedCrust = crust;
-                    });
-                  },
-                  dropdownMenuEntries: PizzaCrust.values
-                      .map<DropdownMenuEntry<PizzaCrust>>((PizzaCrust crust) {
-                    return DropdownMenuEntry<PizzaCrust>(
-                      value: crust,
-                      label: crust.label,
-                    );
-                  }).toList(),
-                ),
-              ],
-            ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Order Pizza'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    sizeController.dispose();
+    sauceController.dispose();
+    crustController.dispose();
+    super.dispose();
   }
 }
