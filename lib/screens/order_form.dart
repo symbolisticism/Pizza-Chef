5cea186f9c89abba2916b47bb4e4f41406914617
Gabriele Peck <peck.gabriele@gmail.com>
2024-10-21 12:06:51 -0700

    completely changed the order form so that it has three dropdown menus now for the size, sauce, and crust

--- lib/screens/order_form.dart
+++ lib/screens/order_form.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pizza_chef/bloc/small_pizza/small_pizza_bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pizza_chef/bloc/small_pizza/small_pizza_event.dart';
-import 'package:pizza_chef/bloc/small_pizza/small_pizza_state.dart';
-import 'package:logger/logger.dart';
-
-var logger = Logger(printer: PrettyPrinter());
+import 'package:pizza_chef/data/pizza_sauce.dart';
+import 'package:pizza_chef/data/pizza_size.dart';
+import 'package:pizza_chef/data/pizza_crust.dart';
 
 class OrderForm extends StatefulWidget {
   const OrderForm({super.key});
@@ -16,68 +12,89 @@ class OrderForm extends StatefulWidget {
 
 class _OrderFormState extends State<OrderForm> {
   final _formKey = GlobalKey<FormState>();
-  final smallPizzaController = TextEditingController();
-  bool secondSection = false;
+  final sizeController = TextEditingController();
+  final sauceController = TextEditingController();
+  final crustController = TextEditingController();
+  PizzaSize? selectedSize;
+  PizzaSauce? selectedSauce;
+  PizzaCrust? selectedCrust;
 
   @override
   void dispose() {
-    smallPizzaController.dispose();
+    sizeController.dispose();
+    sauceController.dispose();
+    crustController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final smallPizzaCounterBloc = context.read<SmallPizzaCounterBloc>();
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Start Order'),
+        title: const Text('Order'),
       ),
       body: Form(
         key: _formKey,
-        child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Row(children: [Text('How many pizzas would you like?')]),
-              BlocBuilder<SmallPizzaCounterBloc, SmallPizzaCounterState>(
-                builder: (context, state) {
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    smallPizzaController.text = state.count.toString();
-                    logger.d(state.count);
+              DropdownMenu<PizzaSize>(
+                initialSelection: PizzaSize.small,
+                controller: sizeController,
+                requestFocusOnTap: true,
+                label: const Text('Pizza Size'),
+                onSelected: (PizzaSize? size) {
+                  setState(() {
+                    selectedSize = size;
                   });
-
-                  return Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          smallPizzaCounterBloc
-                              .add(SmallPizzaDecrement()); // add an event
-                        },
-                        icon: const Icon(Icons.remove),
-                      ),
-                      Expanded(
-                        child: TextFormField(
-                          controller: smallPizzaController,
-                          decoration: const InputDecoration(
-                              label: Text('How many small pizzas?')),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          smallPizzaCounterBloc
-                              .add(SmallPizzaIncrement()); // add an event
-                        },
-                        icon: const Icon(Icons.add),
-                      ),
-                    ],
+                },
+                dropdownMenuEntries: PizzaSize.values
+                    .map<DropdownMenuEntry<PizzaSize>>((PizzaSize size) {
+                  return DropdownMenuEntry<PizzaSize>(
+                    value: size,
+                    label: size.label,
                   );
-                },
+                }).toList(),
               ),
-              TextButton(
-                onPressed: () {
-                  secondSection = true;
+              const SizedBox(height: 48),
+              DropdownMenu<PizzaSauce>(
+                initialSelection: PizzaSauce.red,
+                controller: sauceController,
+                requestFocusOnTap: true,
+                label: const Text('Pizza Sauce'),
+                onSelected: (PizzaSauce? sauce) {
+                  setState(() {
+                    selectedSauce = sauce;
+                  });
                 },
-                child: const Text('Next'),
+                dropdownMenuEntries: PizzaSauce.values
+                    .map<DropdownMenuEntry<PizzaSauce>>((PizzaSauce sauce) {
+                  return DropdownMenuEntry<PizzaSauce>(
+                    value: sauce,
+                    label: sauce.label,
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 48),
+              DropdownMenu<PizzaCrust>(
+                initialSelection: PizzaCrust.thinCrust,
+                controller: crustController,
+                requestFocusOnTap: true,
+                label: const Text('Pizza Crust'),
+                onSelected: (PizzaCrust? crust) {
+                  setState(() {
+                    selectedCrust = crust;
+                  });
+                },
+                dropdownMenuEntries: PizzaCrust.values
+                    .map<DropdownMenuEntry<PizzaCrust>>((PizzaCrust crust) {
+                  return DropdownMenuEntry<PizzaCrust>(
+                    value: crust,
+                    label: crust.label,
+                  );
+                }).toList(),
               ),
             ],
           ),
