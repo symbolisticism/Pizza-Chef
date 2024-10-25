aa2258d731c6f077baadb706d1a0467a45ebe880
Gabriele Peck <peck.gabriele@gmail.com>
2024-10-25 15:24:25 -0700

    added logic to load pizzas from the database, ensured that the properties of the pizza from the database were converted back to their proper type and combined back into pizza objects

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logger/logger.dart';
+import 'package:pizza_chef/data/pizza_crust.dart';
+import 'package:pizza_chef/data/pizza_sauce.dart';
+import 'package:pizza_chef/data/pizza_size.dart';
+import 'package:pizza_chef/models/pizza.dart';
 
-class Cart extends StatelessWidget {
+final db = FirebaseFirestore.instance;
+var logger = Logger(printer: PrettyPrinter());
+
+class Cart extends StatefulWidget {
   const Cart({super.key});
+
+  @override
+  State<Cart> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  Widget? content;
+  Map<String, Pizza> pizzas = {};
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +31,110 @@ class Cart extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cart'),
       ),
+      body: FutureBuilder(
+          future: loadPizzas(),
+          builder: (context, snapshot) {
+            // content is always null
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                logger.e(snapshot.error);
+                return Center(child: Text("${snapshot.error} has occurred."));
+              } else if (snapshot.hasData &&
+                  content != null &&
+                  pizzas.isEmpty) {
+                return const Center(
+                    child: Text("There are no pizzas to display."));
+              } else if (snapshot.hasData &&
+                  content != null &&
+                  pizzas.isNotEmpty) {
+                return content!;
+              } else if (content == null) {
+                return const Center(child: Text('Content is null.'));
+              } else {
+                return const Center(
+                    child: Text('An unexpected error has occurred.'));
+              }
+            }
+
+            return const Center(child: CircularProgressIndicator());
+          }),
     );
+  }
+
+  Future<Widget> loadPizzas() async {
+    final QuerySnapshot querySnapshot = await db.collection('cart').get();
+
+    for (var doc in querySnapshot.docs) {
+      final pizzaId = doc.get('id') as String;
+      dynamic pizzaSize = doc.get('pizzaSize') as String;
+      final toppingsDynamic = doc.get('toppings') as List<dynamic>;
+      dynamic sauce = doc.get('sauce') as String;
+      dynamic crust = doc.get('thinCrust') as String;
+
+      List<String> toppings = List<String>.from(toppingsDynamic);
+
+      // reassign as the right type
+      switch (pizzaSize) {
+        case 'Small':
+          pizzaSize = PizzaSize.small;
+          break;
+        case 'Medium':
+          pizzaSize = PizzaSize.medium;
+          break;
+        case 'Large':
+          pizzaSize = PizzaSize.large;
+          break;
+      }
+
+      // reassign as the right type
+      switch (sauce) {
+        case 'Red':
+          sauce = PizzaSauce.red;
+          break;
+        case 'White':
+          sauce = PizzaSauce.white;
+          break;
+      }
+
+      // reassign as the right type
+      switch (crust) {
+        case 'Thin Crust':
+          crust = PizzaCrust.thinCrust;
+          break;
+        case 'Regular Crust':
+          crust = PizzaCrust.regularCrust;
+          break;
+      }
+
+      if (!pizzas.containsKey(pizzaId)) {
+        pizzas[pizzaId] = Pizza(
+            pizzaSize: pizzaSize,
+            toppings: toppings,
+            sauce: sauce,
+            crustType: crust);
+      }
+      logger.d(pizzas[pizzaId]);
+    }
+
+    logger.d(pizzas);
+
+    return content = Padding(
+      padding: const EdgeInsets.all(16),
+      child: ListView.builder(
+        itemCount: pizzas.length,
+        itemBuilder: (context, index) {
+          return const ListTile(
+            title: Text("Pizza 1"),
+          );
+        },
+      ),
+    );
+
+    // return Padding(
+    //   padding: const EdgeInsets.all(16),
+    //   child: ListView.builder(
+    //     itemCount: ,
+    //     itemBuilder: (context, index) {}),
+    // );
   }
 }
