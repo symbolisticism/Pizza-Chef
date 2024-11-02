5ebdecf469e759bdd54deefb6eb59d6e5b511d1e
Gabriele Peck <peck.gabriele@gmail.com>
2024-11-02 15:29:31 -0700

    added a timestamp property to the pizza object

--- lib/models/pizza.dart
+++ lib/models/pizza.dart
@@ -6,25 +6,33 @@ import 'package:uuid/uuid.dart';
 var uuid = const Uuid();
 
 class Pizza {
-  Pizza(
-      {required this.pizzaSize,
-      required this.toppings,
-      required this.sauce,
-      required this.crustType});
+  Pizza({
+    required this.pizzaSize,
+    required this.toppings,
+    required this.sauce,
+    required this.crustType,
+    required this.timestamp,
+  });
 
   final PizzaSize pizzaSize;
   final List<String> toppings;
   final PizzaSauce sauce;
   final PizzaCrust crustType;
-  final id = uuid.v4();
+  final id = uuid.v1();
+  final DateTime timestamp;
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
       'thinCrust': crustType.label,
       'sauce': sauce.label,
       'toppings': toppings,
-      'pizzaSize': pizzaSize.label
+      'pizzaSize': pizzaSize.label,
+      'timestamp' : timestamp,
     };
+  }
+
+  @override
+  String toString() {
+    return "$pizzaSize pizza with $sauce sauce, $crustType, and the following toppings: $toppings";
   }
 }
