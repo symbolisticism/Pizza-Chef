504d3f518363c954f33c75cff09927293f5ff6dd
Gabriele Peck <peck.gabriele@gmail.com>
2024-11-12 10:15:06 -0700

    added logic to deliver the last saved screen in the state saved in Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -13,77 +13,182 @@ import 'package:logger/logger.dart';
 var db = FirebaseFirestore.instance;
 var logger = Logger(printer: PrettyPrinter());
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  CollectionReference state = db.collection('state');
-  QuerySnapshot snapshot = await state.get();
-
-  String screen = 'home';
-  PizzaSize? size;
-  PizzaSauce? sauce;
-  PizzaCrust? crust;
-  List<String>? toppings;
-  dynamic lastOpened;
-
-  int currentTimestamp = DateTime.now().millisecondsSinceEpoch;
-  int fiveMinutesInMs = 10 *
-      1000; // TODO: change this to actually be 5 minutes once functionality is confirmed
-
-  // TODO: In the case that the person started customizing a pizza but didn't ever
-  // finish, the state should be cleared here (maybe)
-
-  for (var doc in snapshot.docs) {
-    // check if the user's been away for longer than five minutes
-    lastOpened = doc.get('lastOpened');
-
-    try {
-      lastOpened = int.parse(lastOpened);
-    } catch (e) {
-      logger.e(e);
-    }
-
-    logger.d(lastOpened.runtimeType);
-
-    if (currentTimestamp - lastOpened > fiveMinutesInMs) break;
-
-    // if the user is returning after a short break, restore the previous state
-    screen = doc.get('screen');
-
-    if (screen == 'order form') {
-      size = doc.get('size');
-      sauce = doc.get('sauce');
-      crust = doc.get('crust');
-      toppings = doc.get('toppings');
-    }
-  }
-
-  // runApp(
-  //   MaterialApp(
-  //     debugShowCheckedModeBanner: false,
-  //     home: screen == 'cart'
-  //         ? const Cart()
-  //         : screen == 'order form'
-  //             ? OrderForm(
-  //                 selectedSize: size!,
-  //                 selectedCrust: crust!,
-  //                 selectedSauce: sauce!,
-  //                 selectedToppings: toppings!,
-  //               )
-  //             : const Home(),
-  //   ),
-  // );
-
+void main() {
   runApp(MaterialApp(
+    routes: {
+      '/home': (context) => const Home(),
+      '/cart': (context) => const Cart(),
+      '/order': (context) => OrderForm.defaultOrder(),
+    },
     debugShowCheckedModeBanner: false,
-    home: FutureBuilder(future: loadState, builder: (context, snapshot) {}),
+    home: const MyApp(),
   ));
 }
 
-Future<Map<String, String?>> loadState() async {
-  return {'hello': null};
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<Map<String, dynamic>> lastState;
+  bool initializationComplete = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // app initialization
+    WidgetsFlutterBinding.ensureInitialized();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const Home()),
+          (route) => false);
+    });
+    initializeFirebase().then((_) {
+      lastState = loadState();
+      setState(() {
+        initializationComplete = true;
+      });
+    });
+
+    // load in state
+    // lastState = loadState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!initializationComplete) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: FutureBuilder<Map<String, dynamic>>(
+          future: lastState,
+          builder: (context, snapshot) {
+            // error
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            }
+
+            if (snapshot.hasData) {
+              Map<String, dynamic>? data = snapshot.data;
+
+              if (data != null) {
+                shouldResetLastState(data['lastOpened']);
+
+                // TODO: ensure the home screen is always on the bottom of the stack
+                // TODO: currently, the app remembers the state and delivers the
+                // TODO: relevant screen, but if it delivers the cart or order
+                // TODO: form page, there is no way to navigate back to home
+
+                switch (data['screen']) {
+                  case 'cart':
+                    return const Cart();
+                  case 'order form':
+                    // convert values back to correct data types
+                    Map<String, dynamic> newPizzaValues =
+                        convertPizzaValues(data);
+
+                    return OrderForm(
+                        selectedSize: newPizzaValues['size'],
+                        selectedSauce: newPizzaValues['sauce'],
+                        selectedCrust: newPizzaValues['crust'],
+                        selectedToppings: newPizzaValues['toppings']);
+                  default:
+                    return const Home();
+                }
+              } else {
+                return const Center(child: Text('Data is null.'));
+              }
+            }
+            return const Center(child: CircularProgressIndicator());
+          }),
+    );
+  }
+
+  Future<Map<String, dynamic>> loadState() async {
+    QuerySnapshot querySnapshot = await db.collection('state').get();
+    Map<String, dynamic> lastState = {};
+
+    for (var doc in querySnapshot.docs) {
+      lastState['screen'] = doc.get('screen');
+      lastState['pizzaValues'] = doc.get('pizzaValues');
+      lastState['lastOpened'] = doc.get('lastOpened');
+    }
+
+    return lastState;
+  }
+
+  Future<void> initializeFirebase() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  bool shouldResetLastState(int timestamp) {
+    int currentTimestamp = DateTime.now().millisecondsSinceEpoch;
+    int fiveMinutes = 10 *
+        1000; // TODO: change this back to five minutes after testing, currently 10 seconds
+    int timeElapsed = currentTimestamp - timestamp;
+
+    if (timeElapsed > fiveMinutes) {
+      resetState();
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<void> resetState() async {
+    // current time
+    int currentTime = DateTime.now().millisecondsSinceEpoch;
+    Map<String, dynamic> resetValues = {
+      'lastOpened': currentTime,
+      'screen': 'home',
+      // TODO: determine if null is the best value to reset the values to
+      'pizzaValues': {
+        'crust': 'thin crust',
+        'sauce': 'red',
+        'size': 'small',
+        'toppings': [],
+      }
+    };
+    await db.collection('state').doc('1').set(resetValues);
+  }
+
+  Map<String, dynamic> convertPizzaValues(Map<String, dynamic> pizzaValues) {
+    PizzaSize size = pizzaValues['size'] == 'small'
+        ? PizzaSize.small
+        : pizzaValues['size'] == 'medium'
+            ? PizzaSize.medium
+            : PizzaSize.large;
+
+    PizzaSauce sauce =
+        pizzaValues['sauce'] == 'red' ? PizzaSauce.red : PizzaSauce.white;
+
+    PizzaCrust crust = pizzaValues['crust'] == 'thin crust'
+        ? PizzaCrust.thinCrust
+        : PizzaCrust.regularCrust;
+
+    dynamic toppings = pizzaValues['toppings'];
+    toppings = List<String>.from(toppings);
+    toppings = (toppings..sort()).toList();
+
+    return {
+      'size': size,
+      'crust': crust,
+      'sauce': sauce,
+      'toppings': toppings,
+    };
+  }
 }
