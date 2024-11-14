0249f9b4e051866026f9a40052f62608af90920a
Gabriele Peck <peck.gabriele@gmail.com>
2024-11-13 17:19:09 -0700

    added shopping cart with badge of how many items to the nav_drawer

--- lib/widgets/nav_drawer.dart
+++ lib/widgets/nav_drawer.dart
@@ -1,12 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:pizza_chef/screens/cart.dart';
+import 'package:pizza_chef/widgets/shopping_cart_badge.dart';
 
 var logger = Logger(printer: PrettyPrinter());
 
-class NavDrawer extends StatelessWidget {
+class NavDrawer extends StatefulWidget {
   const NavDrawer(this.currentScreenName, {super.key});
 
   final String currentScreenName;
+
+  @override
+  State<NavDrawer> createState() => _NavDrawerState();
+}
+
+class _NavDrawerState extends State<NavDrawer> {
+  late final String currentScreenName;
+  final dbStream = FirebaseFirestore.instance.collection('cart').snapshots();
+
+  @override
+  void initState() {
+    super.initState();
+    currentScreenName = widget.currentScreenName;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +35,7 @@ class NavDrawer extends StatelessWidget {
           TextButton.icon(
             key: const Key('homeButton'),
             onPressed: () {
-              if (currentScreenName != '/home') {
+              if (widget.currentScreenName != '/home') {
                 Navigator.pop(context);
                 Navigator.pushReplacementNamed(context, '/home');
               } else {
@@ -32,7 +49,7 @@ class NavDrawer extends StatelessWidget {
           TextButton.icon(
             key: const Key('orderButton'),
             onPressed: () {
-              if (currentScreenName != '/order') {
+              if (widget.currentScreenName != '/order') {
                 Navigator.pop(context);
                 Navigator.pushReplacementNamed(context, '/order');
               } else {
@@ -45,21 +62,39 @@ class NavDrawer extends StatelessWidget {
           const SizedBox(height: 20),
           // TODO: See if I can replace this with the shopping cart icon
           // that's currently on the home screen
-          TextButton.icon(
-            key: const Key('cartButton'),
-            onPressed: () {
-              if (currentScreenName != '/cart') {
-                Navigator.pop(context);
-                Navigator.pushReplacementNamed(context, '/cart');
-              } else {
-                Navigator.pop(context);
+          StreamBuilder<QuerySnapshot>(
+            stream: dbStream,
+            builder: (context, snapshot) {
+              int itemCount = 0;
+              if (snapshot.hasData) {
+                itemCount = snapshot.data!.docs.length;
               }
+
+              return TextButton.icon(
+                key: const Key('cartButton'),
+                label: const Text('Cart'),
+                onPressed: () {
+                  if (widget.currentScreenName != '/cart') {
+                    Navigator.pop(context);
+                    Navigator.pushReplacementNamed(context, '/cart');
+                  } else {
+                    Navigator.pop(context);
+                  }
+                },
+                icon: ShoppingCartWithBadge(itemCount: itemCount),
+              );
             },
-            label: const Text('Cart'),
-            icon: const Icon(Icons.shopping_cart),
           ),
         ],
       ),
     );
   }
+}
+
+void navigateToCart(context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => const Cart(),
+    ),
+  );
 }
